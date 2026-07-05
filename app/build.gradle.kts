2d44f6e239631aea44ae7c8479ea443409b9364f
綦桐 <qitong@example.com>
2026-07-05 14:26:09 +0000

    🚀 v3.5.6 指示灯：红绿灯显示测速状态

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.qtwl.gateway"
         minSdk = 24
         targetSdk = 35
-versionCode = 76
-        versionName = "3.5.5"
+versionCode = 77
+        versionName = "3.5.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
