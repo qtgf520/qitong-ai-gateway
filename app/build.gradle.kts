c65430d6c3457167a4d64cabbcbceea9777be490
綦桐 <qitong@example.com>
2026-07-06 01:06:02 +0000

    🚀 v3.5.9 前缀指令+绑定脑子+故障转移保留

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.qtwl.gateway"
         minSdk = 24
         targetSdk = 35
-versionCode = 79
-        versionName = "3.5.8"
+versionCode = 80
+        versionName = "3.5.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
